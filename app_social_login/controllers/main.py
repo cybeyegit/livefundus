a4e717dc62c5068f54b10dc6fa61c4d57ea8b03e
MakeBigNews <qding1990@gmail.com>
2020-07-02 22:21:24 -0700

    allow all users in user list

--- app_social_login/controllers/main.py
+++ app_social_login/controllers/main.py
@@ -24,20 +24,17 @@ class WebsiteContact(WebsiteProfile):
             return user_sudo
         return user_sudo
 
+
     @http.route(['/profile/users',
                  '/profile/users/page/<int:page>'], type='http', auth="public", website=True)
     def view_all_users_page(self, page=1, **searches):
         User = request.env['res.users']
-        dom = []
+        dom = [('website_published', '=', True)]
 
         # Searches
         search_term = searches.get('search')
         if search_term:
-            dom = expression.AND(
-                [['|', ('name', 'ilike', search_term), ('company_id.name', 'ilike', search_term)], dom])
-
-        if not search_term:
-            dom.append(('is_published', '=', True))
+            dom = expression.AND([['|', ('name', 'ilike', search_term), ('company_id.name', 'ilike', search_term)], dom])
 
         user_count = User.sudo().search_count(dom)
 
@@ -46,19 +43,21 @@ class WebsiteContact(WebsiteProfile):
             pager = request.website.pager(url="/profile/users", total=user_count, page=page, step=self._users_per_page,
                                           scope=page_count if page_count < self._pager_max_pages else self._pager_max_pages)
 
-            users = User.sudo().search(dom, limit=self._users_per_page, offset=pager['offset'],
-                                       order='karma DESC, create_date DESC')
+            users = User.sudo().search(dom, limit=self._users_per_page, offset=pager['offset'], order='karma DESC')
             user_values = self._prepare_all_users_values(users)
 
+            # Get karma position for users (only website_published)
+            position_domain = [ ('website_published', '=', True)]
+            position_map = self._get_users_karma_position(position_domain, users.ids)
+            for user in user_values:
+                user['position'] = position_map.get(user['id'], 0)
+
             values = {
-                'users': user_values if not search_term and page == 1 else user_values,
+                'top3_users': user_values[:3] if not search_term and page == 1 else None,
+                'users': user_values[3:] if not search_term and page == 1 else user_values,
                 'pager': pager
             }
         else:
-            values = {
-                'users': [],
-                'search': search_term,
-                'pager': dict(page_count=0)
-            }
+            values = {'top3_users': [], 'users': [], 'search': search_term, 'pager': dict(page_count=0)}
 
         return request.render("website_profile.users_page_main", values)

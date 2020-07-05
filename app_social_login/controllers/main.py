fea9026adee5aeaf5d8c1059ed0fd96f987e0500
MakeBigNews <qding1990@gmail.com>
2020-07-05 13:47:15 -0700

    add events tab

--- app_social_login/controllers/main.py
+++ app_social_login/controllers/main.py
@@ -5,10 +5,11 @@ import logging
 from odoo.addons.website_profile.controllers.main import WebsiteProfile
 
 import math
-
-from odoo import http, modules, tools
+import babel.dates
+from odoo import http, modules, tools, fields, _
 from odoo.http import request
 from odoo.osv import expression
+from odoo.tools.misc import get_lang
 
 _logger = logging.getLogger(__name__)
 
@@ -25,14 +26,27 @@ class WebsiteContact(WebsiteProfile):
             return user_sudo
         return user_sudo
 
+    def get_formated_date(self, event):
+        start_date = fields.Datetime.from_string(event.date_begin).date()
+        end_date = fields.Datetime.from_string(event.date_end).date()
+        month = babel.dates.get_month_names('abbreviated', locale=get_lang(event.env).code)[start_date.month]
+        return ('%s %s%s') % (
+        month, start_date.strftime("%e"), (end_date != start_date and ("-" + end_date.strftime("%e")) or ""))
 
     def _prepare_user_profile_values(self, user, **post):
-        values = super(WebsiteProfile, self)._prepare_user_profile_values(user, **post)
+        values = super(WebsiteContact, self)._prepare_user_profile_values(user, **post)
 
-        values['user_events'] = request.env['event.event'].sudo().browse({'created_by':user.id})
+        events = request.env['event.event'].sudo().search([('create_uid', '=', user.id)])
+
+        values['user_events'] = []
+
+        for event in events:
+            values['user_events'].append({
+                "date": self.get_formated_date(event),
+                "event": event,
+                "url": event.website_url})
 
         return values
-
 
     @http.route(['/profile/users',
                  '/profile/users/page/<int:page>'], type='http', auth="public", website=True)
@@ -43,7 +57,8 @@ class WebsiteContact(WebsiteProfile):
         # Searches
         search_term = searches.get('search')
         if search_term:
-            dom = expression.AND([['|', ('name', 'ilike', search_term), ('company_id.name', 'ilike', search_term)], dom])
+            dom = expression.AND(
+                [['|', ('name', 'ilike', search_term), ('company_id.name', 'ilike', search_term)], dom])
 
         user_count = User.sudo().search_count(dom)
 
@@ -56,7 +71,7 @@ class WebsiteContact(WebsiteProfile):
             user_values = self._prepare_all_users_values(users)
 
             # Get karma position for users (only website_published)
-            position_domain = [ ('website_published', '=', True)]
+            position_domain = [('website_published', '=', True)]
             position_map = self._get_users_karma_position(position_domain, users.ids)
             for user in user_values:
                 user['position'] = position_map.get(user['id'], 0)
